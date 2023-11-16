c05b97718cfda956eb95cba2fbf6d5e05dd2c823
nikolai.in <nikolai-in@dousen.ru>
2023-11-16 11:35:13 +0300

    ♻️ Refactor code to improve configuration loading and add main entry point

--- aeza1password/__init__.py
+++ aeza1password/__init__.py
@@ -7,9 +7,21 @@ from dotenv import load_dotenv
 
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
 
-load_dotenv()
-value = getenv("APIKEY")
-if type(value) is str:
-    api_keys = value.split(",")
 
-logging.debug(f"API Keys: {api_keys}")
+def load_config():
+    """Load configuration from .env file"""
+    load_dotenv(".aeza1password.env")
+    value = getenv("APIKEY")
+    if type(value) is str:
+        api_keys = value.split(",")
+        logging.debug(f"Loaded {len(api_keys)} API keys")
+
+
+def main():
+    """Main entry point of the app"""
+    logging.debug("Starting aeza1password")
+
+
+if __name__ == "__main__":
+    load_config()
+    main()
