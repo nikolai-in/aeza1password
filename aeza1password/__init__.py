8817b987d4c908804c61f9001295fe37458b6834
nikolai.in <nikolai-in@dousen.ru>
2023-11-16 04:43:14 +0300

    🚧 Add logging and dotenv functionality, update packaging

--- aeza1password/__init__.py
+++ aeza1password/__init__.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python
 """aeza1password — CLI tool for syncing servers from aeza.net to 1password"""
+import logging
+from os import getenv
+
+from dotenv import load_dotenv
+
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
+
+load_dotenv()
+value = getenv("APIKEY")
+if type(value) is str:
+    api_keys = value.split(",")
+
+logging.debug(f"API Keys: {api_keys}")
